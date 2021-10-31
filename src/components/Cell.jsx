7f45acdbb2e90236498cfa117288121cb9ee7f11
Utqrsh04 <utkarshrkt2001@gmail.com>
2021-10-31 23:53:13 +0530

    ✨ Feat : Added Required Dependencies

---
+++ src/components/Cell.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cell = () => {
+    return (
+        <div>
+            Cell
+        </div>
+    )
+}
+
+export default Cell
---
+++ src/components/Tile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tile = () => {
+    return (
+        <div>
+            Tile
+        </div>
+    )
+}
+
+export default Tile
